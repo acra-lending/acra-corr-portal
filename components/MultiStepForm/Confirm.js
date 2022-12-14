4c18057460c3e148e121d54049d3d1035620e2f4
Chris Liu <chrisl@CSC.local>
2022-12-14 13:29:52 -0800

    Add loader spinner

--- components/MultiStepForm/Confirm.js
+++ components/MultiStepForm/Confirm.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from 'axios';
+import {Oval} from "react-loader-spinner";
 
 export default function Confirm(props) {
   const [isSubmitting, setIsSubmitting] = useState();
