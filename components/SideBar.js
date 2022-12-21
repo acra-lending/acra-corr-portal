1222c8334dff68c109b6d3ccc9d54970b6d822c9
Chris Liu <chrisl@CSC.local>
2022-12-21 10:35:25 -0800

    Fix sidebar scrollbar

--- components/SideBar.js
+++ components/SideBar.js
@@ -15,7 +15,7 @@ export default function SideBar ({ props }) {
     const router = useRouter(); 
 
     //Classnames for div and tabs
-    const sideBarDeskTopClassNames = 'min-h-screen min-w-fit bg-white w-64 z-10 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out border-r overflow-y-scroll';
+    const sideBarDeskTopClassNames = 'min-h-screen min-w-fit bg-white w-64 z-10 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out border-r';
     const sideBarMobileClassNames = 'min-h-screen min-w-fit z-40 bg-white fixed inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out border-r shadow-md overflow-y-scroll';
     const activeTabClassNames = 'flex items-center gap-3 bg-[#0033A1] text-white p-3 rounded text-sm';
     const tabClassNames = 'flex items-center gap-3 p-3 rounded hover:bg-[#f3f4f6] text-sm';
